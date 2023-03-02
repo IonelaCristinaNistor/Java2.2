befa119395be3d9bc08df2c1955e1afc578efd44
stud1pj <student1.pj@gmail.com>
2023-03-02 11:33:25 +0200

    dd

--- src/ex1/ex1.java
+++ src/ex1/ex1.java
@@ -1,2 +1,29 @@
-package ex1;public class ex1 {
+/*1. Fișierul judete_in.txt, conține lista neordonată a județelor din țară. Să se încarce
+datele din fișier într-un tablou de String-uri și să se ordoneze acest tablou cu ajutorul metodei
+sort() din clasa Arrays. Să se returneze pe ce poziție se află în vectorul ordonat un județ
+introdus de la tastatură. Se va utiliza metoda de căutare binară din clasa Arrays*/
+package ex1;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+public class ex1 {
+    public static void main(String[] args) throws IOException{
+        String fisier = "judete_in.txt";
+        BufferedReader flux_in;
+        flux_in = new BufferedReader(new InputStreamReader((new FileInputStream("src/ex1/judete_in.txt"))));
+        String linie;
+        String[] judete = new String[7];
+        int i = 0;
+        while((linie = flux_in.readLine()) != null)
+        {
+            judete[i] = linie;
+            //System.out.println(linie);
+           i++;
+        }
+        Arrays.sort(judete);
+        System.out.println(judete);
+    }
 }
--- src/ex2/ex2.java
+++ src/ex2/ex2.java
@@ -1,2 +1,32 @@
-package ex2;public class ex2 {
+/*2. Fișierul cantec_in.txt conține versurile unui cântec la alegere. Să se scrie un
+program care creează fișierul cantec_out.txt, care conține versurile cântecului original dar în
+plus în dreptul fiecărui rând sunt afișate numărul de cuvinte de pe rând şi numărul de vocale
+de pe fiecare rând. În dreptul rândurilor care se încheie cu o grupare de litere aleasă se va
+pune o steluță (*). Rândurile pentru care un număr generat aleator este mai mic decât 0.1 vor
+fi scrise cu majuscule (se vor genera aleator numere între 0 şi 1).
+Se va defini o clasă Vers, care are o variabilă membră privată un șir de caractere
+reprezentând versul și se va dezvolta câte un operator pentru fiecare cerință de mai sus (o
+metodă care returnează numărul de cuvinte, o metodă care returnează numărul de vocale, etc).
+Se va crea un vector de obiecte de tip Vers care va conține informația preluată din fișierul de
+intrare. */
+package ex2;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class ex2 {
+
+    public static void main(String[] args) throws IOException {
+        String fisier = "cantec_in.txt";
+        BufferedReader flux_in;
+        flux_in = new BufferedReader(new InputStreamReader((new FileInputStream("src/ex2/cantec_in.txt"))));
+        String linie;
+        while ((linie = flux_in.readLine()) != null) {
+
+        }
+    }
 }
+
